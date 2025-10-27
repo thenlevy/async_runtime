100b688b3d5431e849cc8ae61c806017866de194
thenlevy <nicolas@stockly.ai>
2025-10-27 23:48:51 +0100

    Waker

--- src/a_la_mano/executor.rs
+++ src/a_la_mano/executor.rs
@@ -1,13 +1,16 @@
 //! https://redixhumayun.github.io/async/2024/10/10/async-runtimes-part-iii.html
-use std::{
-    cell::RefCell,
-    pin::Pin,
-    rc::Rc,
-    sync::mpsc::{self, Receiver, Sender},
+use {
+    core::task::{RawWaker, RawWakerVTable, Waker},
+    std::{
+        cell::RefCell,
+        pin::Pin,
+        rc::Rc,
+        sync::mpsc::{self, Receiver, Sender},
+    },
 };
 
 struct Executor {
-    tasks: Rc<RefCell<TaskQueue>>,
+    task_queue: Rc<RefCell<TaskQueue>>,
     next_task_id: usize,
 }
 
@@ -106,4 +109,75 @@ impl TaskQueue {
 pub struct Task {
     pub id: usize,
     pub future: RefCell<Pin<Box<dyn Future<Output = ()> + 'static>>>,
+}
+
+pub struct MyWaker {
+    task: Rc<Task>,
+    sender: Sender<Rc<Task>>,
+}
+
+impl MyWaker {
+    const VTABLE: RawWakerVTable =
+        RawWakerVTable::new(Self::clone, Self::wake, Self::wake_by_ref, Self::drop);
+
+    pub fn new(task: Rc<Task>, sender: Sender<Rc<Task>>) -> Waker {
+        let pointer = Rc::into_raw(Rc::new(MyWaker { task, sender })) as *const ();
+        let vtable = &MyWaker::VTABLE;
+        unsafe { Waker::from_raw(RawWaker::new(pointer, vtable)) }
+    }
+
+    unsafe fn clone(ptr: *const ()) -> RawWaker {
+        let waker = std::mem::ManuallyDrop::new(unsafe {
+            // SAFETY:
+            // * `ptr` was created either from MyWaker::new or MyWaker::clone. In both cases,
+            // it was previously returned by a call to Rc<MyWaker>::into_raw.
+            // * The reference count is
+            //  - Set to 1 on creation in MyWaker::new (by Rc::<MyWaker>::new).
+            //  - Incremented by 1 in MyWaker::clone below
+            //   - Decremented by 1 when either the Waker is dropped and calls MyWaker::drop or when
+            //     the Waker calls MyWaker::wake which transfers ownership to the wake function.
+            Rc::from_raw(ptr as *const MyWaker)
+        });
+        let cloned_waker = Rc::clone(&waker);
+        let raw_pointer = Rc::into_raw(cloned_waker);
+        RawWaker::new(raw_pointer as *const (), &Self::VTABLE)
+    }
+
+    unsafe fn wake(ptr: *const ()) {
+        let waker = unsafe {
+            // SAFETY:
+            // * `ptr` was created either from MyWaker::new or MyWaker::clone. In both cases,
+            // it was previously returned by a call to Rc<MyWaker>::into_raw.
+            // * See the implementation of MyWaker::clone for details on why the value is dropped
+            //   only once.
+            Rc::from_raw(ptr as *const MyWaker)
+        };
+        waker.sender.send(Rc::clone(&waker.task)).unwrap();
+        // `waker` is dropped here, decrementing the reference count by 1.
+        // This is intended as `wake` takes ownership of the waker.
+    }
+
+    unsafe fn wake_by_ref(ptr: *const ()) {
+        let waker = unsafe {
+            // SAFETY:
+            // * `ptr` was created either from MyWaker::new or MyWaker::clone. In both cases,
+            // it was previously returned by a call to Rc<MyWaker>::into_raw.
+            // * See the implementation of MyWaker::clone for details on why the value is dropped
+            //   only once.
+            &*(ptr as *const MyWaker)
+        };
+        waker.sender.send(Rc::clone(&waker.task)).unwrap();
+        // In this context, the ressources are borrowed, so we don't decrement the reference count.
+    }
+
+    unsafe fn drop(ptr: *const ()) {
+        drop(unsafe {
+            // SAFETY:
+            // * `ptr` was created either from MyWaker::new or MyWaker::clone. In both cases,
+            // it was previously returned by a call to Rc<MyWaker>::into_raw.
+            // * See the implementation of MyWaker::clone for details on why the value is dropped
+            //   only once.
+            Rc::from_raw(ptr as *const MyWaker)
+        });
+    }
 }
