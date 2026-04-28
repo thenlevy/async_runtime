a1ef3af2b327eb7456d06042ed52d0d7787ee54e
thenlevy <nicolas@stockly.ai>
2026-04-28 17:41:04 +0200

    no rc

--- src/a_la_mano/mod.rs
+++ src/a_la_mano/mod.rs
@@ -4,8 +4,6 @@ mod tcp;
 
 use executor::Executor;
 
-use std::rc::Rc;
-
 async fn run() {
     let tcp_listener =
         tcp::AsyncTcpListener::bind("127.0.0.1:8080").expect("Failed to bind TCP listener");
@@ -16,16 +14,16 @@ async fn run() {
             .await
             .expect("Failed to accept TCP connection");
 
-        let stream = Rc::new(
-            tcp::AsyncTcpStream::from_tcp_stream(tcp_stream).expect("Failed to wrap stream"),
-        );
+        let stream =
+            tcp::AsyncTcpStream::from_tcp_stream(tcp_stream).expect("Failed to wrap stream");
+
         if let Err(e) = Executor::spawn(handle_connection(stream)) {
             eprintln!("Failed to spawn task: {e}");
         }
     }
 }
 
-async fn handle_connection(stream: Rc<tcp::AsyncTcpStream>) {
+async fn handle_connection(stream: tcp::AsyncTcpStream) {
     let mut lines = stream.get_lines();
 
     while let Some(line) = lines.next().await {
